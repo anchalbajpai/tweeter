4ff47dbf15a8309ccf14d4386544400d6646df95
anchalbajpai <126510358+anchalbajpai@users.noreply.github.com>
2024-06-25 14:04:19 +0530

    Microblogging app
    Doesn't have the static folder

---
+++ tclone/tweet/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm, UserRegistrationForm
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.db.models import Q
+
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweet_list.html', {'tweets': tweets})
+
+@login_required
+def tweet_create(request):
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid:
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+
+    else:
+        form = TweetForm()
+    return render(request, 'tweet_form.html', {'form': form})
+
+@login_required
+def tweet_edit(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES, instance=tweet)
+        if form.is_valid:
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request, 'tweet_form.html', {'form': form})
+
+@login_required
+def tweet_delete(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == 'POST':
+        tweet.delete()
+        return redirect('tweet_list')
+    return render(request, 'tweet_delete_confirm.html', {'tweet': tweet})
+
+def register(request):
+    if request.method == 'POST':
+        form=UserRegistrationForm(request.POST)
+        if form.is_valid:
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            login(request,user)
+            return redirect('tweet_list')
+    else:
+        form = UserRegistrationForm()
+    
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def search(request):
+    if request.method == 'POST':
+        search = request.POST['search']
+        
+        tweet = Tweet.objects.filter(text__contains=search)
+    else:
+        tweet = Tweet.objects.all() 
+    context = {
+        'tweet':tweet
+    }
+    return render(request,'search.html',context)
